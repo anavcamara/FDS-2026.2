a276e0955ddac0fcdf80dd0ccec6520af76f67bf
Ana Luiza Câmara <alvc@cesar.school>
2026-09-03 08:45:15 -0300

    Adiciona detalhes ao template HTML e redireciona URLs para a página forum

--- project/urls.py
+++ project/urls.py
@@ -22,7 +22,6 @@ from django.views.generic.base import RedirectView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("hello/", include("hello.urls")),
-    path('forum/', include("forum/urls")),  
-    path('forum/', include('forum.urls')),
+    path('forum/', include("forum.urls")),  
     path('', RedirectView.as_view(url='/forum/', permanent=False), name='home'),  # Redirect root URL to forum
 ]
